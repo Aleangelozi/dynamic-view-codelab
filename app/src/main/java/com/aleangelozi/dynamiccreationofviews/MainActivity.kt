b6aaecbfbc9d6c37826df93e4b7d3f671568b5fa
aleangelozi <aleangelozi@gmail.com>
2022-02-24 15:16:00 -0300

    Initial commit

---
+++ app/src/main/java/com/aleangelozi/dynamiccreationofviews/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.aleangelozi.dynamiccreationofviews
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.LinearLayout
+
+class MainActivity : AppCompatActivity() {
+    private var parentLinearLayout: LinearLayout? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        title = "KotlinApp"
+        parentLinearLayout = findViewById(R.id.parent_linear_layout)
+    }
+    fun onDelete(view: View) {
+        parentLinearLayout!!.removeView(view.parent as View)
+    }
+    fun onAddField(view: View) {
+        val inflater =
+            getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val rowView: View = inflater.inflate(R.layout.field, null)
+        parentLinearLayout!!.addView(rowView, parentLinearLayout!!.childCount - 1)
+    }
+}
